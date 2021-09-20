b618a6acf0b98f8976bf8febe6327008a63c95f2
AhmadZulfikarFauzii <91074447+AhmadZulfikarFauzii@users.noreply.github.com>
2021-09-20 22:35:06 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Ahmad Zulfikar Fauzi
+            Saya Lahir Di Provinsi Jambi
+            Hobi saya membuat Design Grafis dan Fotografer";
+    }
+}
